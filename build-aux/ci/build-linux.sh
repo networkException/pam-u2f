7f36d146b7c5d885975aa2dcf719f3bef41769ed
pedro martelletto <pedro@yubico.com>
2021-05-19 10:29:28 +0200

    debug

--- build-aux/ci/build-linux.sh
+++ build-aux/ci/build-linux.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
-set -ex
+set -x
 
 BUILDROOT="$(git rev-parse --show-toplevel)"
 
 pushd "$BUILDROOT" &>/dev/null
+  pkg-config --list-all
+  pkg-config --cflags libfido2
+  pkg-config --libs libfido2
+  pkg-config --cflags libcbor
+  pkg-config --libs libcbor
   ./autogen.sh
   ./configure --disable-silent-rules --disable-man
   make check
